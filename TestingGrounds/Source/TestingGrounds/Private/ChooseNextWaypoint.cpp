a185c8115c409c22adca8b680d12bc5e33a3ddc3
evilagram <evilagram@yahoo.com>
2017-10-19 21:37:43 -0400

    Recreated waypoint behavior in C++, and moved off to new actor component that contains each guard's patrol route.

--- TestingGrounds/Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++ TestingGrounds/Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
@@ -1,13 +1,36 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h" 
+#include "PatrolRoute.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	
-	
-	
-	//UE_LOG(LogTemp,Warning,TEXT("Succeeded!"))
+
+	//get patrol points
+	auto ControllingPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControllingPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if(PatrolPoints.Num() == 0){ 
+		auto Name = FString(ControllingPawn->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("No Patrol Points in Patrol Route for  %s"), *Name)
+		return EBTNodeResult::Failed;
+	}
+
+	//set next waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// TODO, protect against empty patrol routes
+
+	//increment waypoint
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+	//UE_LOG(LogTemp,Warning,TEXT("Waypoint index: %i"), IndexKey)
 
 	return EBTNodeResult::Succeeded;
 }
--- TestingGrounds/Source/TestingGrounds/Public/ChooseNextWaypoint.h
+++ TestingGrounds/Source/TestingGrounds/Public/ChooseNextWaypoint.h
@@ -13,8 +13,16 @@ UCLASS()
 class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
-	
-		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
-	
+public:
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
+
 };
